fa05ba3e590e789d60a010e512846e85d9e8a4f3
Lucianopn <luciano.pn@gmail.com>
2017-01-26 18:08:39 -0200

    Update Exercicio5.java

--- fj11/cap03/Exercicio5.java
+++ fj11/cap03/Exercicio5.java
@@ -2,13 +2,21 @@ package luciano.cursojava.caelun.fj11.cap03;
 
 public class Exercicio5 {
 	
-			public static void main (String[] args) {
-				
-						long fatorial = 1;
-						for(long n = 1; n <=40;n++){
-							fatorial*=n;
-							System.out.println("O fatorial de " + n + " é (" + (n-1) + "!) * " + n + " = " + fatorial);
-						}		
-					
-			}
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite um nÃºmero:");
+        int num = scan.nextInt();
+        
+        System.out.print(num + "! = ");
+        
+        int fatorial = 1;
+        for (int i=num; i>0; i--){
+            fatorial *= i;
+            System.out.print(i+ ".");
+        }
+        
+        System.out.println("Resultado: " + fatorial);
+    }
 }
